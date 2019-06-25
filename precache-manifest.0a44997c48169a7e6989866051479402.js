2442a2cb2d7d9bc5816b8a214d25597866c5faf1
salehmoosapour <salehmoosapour1377@gmail.com>
2019-06-25 17:22:47 +0430

    First Commit

---
+++ precache-manifest.0a44997c48169a7e6989866051479402.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a27f8b7157d65be4723d8a921be091e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc12c1e028e6902c4918",
+    "url": "/static/css/main.11fe1220.chunk.css"
+  },
+  {
+    "revision": "a95f0a946ac2a14e2bbe",
+    "url": "/static/js/2.136689cc.chunk.js"
+  },
+  {
+    "revision": "fc12c1e028e6902c4918",
+    "url": "/static/js/main.7d6c3ab5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c15f384091fe98f299fbaafbdc9ca633",
+    "url": "/static/media/ajax-loader.c15f3840.gif"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
